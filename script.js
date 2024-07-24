9a9a5e9e11b490f2424e5f568e51d7196d7ef6f6
NikoWD <mikolaj.wd0821@gmail.com>
2024-07-24 23:38:48 +0200

    remove child + add liseners

--- script.js
+++ script.js
@@ -174,8 +174,35 @@
 // console.log(btn);
 // --------------
 
-const innerT = document.querySelector(".inner-text");
-const textC = document.querySelector(".text-content");
+// const innerT = document.querySelector(".inner-text");
+// const textC = document.querySelector(".text-content");
 
-console.log(innerT.innerText);
-console.log(textC.textContent);
+// console.log(innerT.innerText);
+// console.log(textC.textContentпше );
+
+// ___________________
+// remove child
+
+// const div_1 = document.querySelector(".div_01");
+// const h1_01 = document.querySelector("h1");
+// console.log(div_1);
+
+// div_1.removeChild(h1_01);
+// _______________
+// addEventListener
+
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+
+btn1.addEventListener("click", function () {
+	console.log("kliknięto mnie");
+});
+
+btn2.addEventListener("mouseover", () => console.log("najechano na mnie"));
+
+const test = () => {
+	console.log("double click");
+};
+
+btn3.addEventListener("dblclick", test);
