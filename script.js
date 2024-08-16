ced39ee6e200176caea8b799c7a5f24ef96b333f
NikoWD <mikolaj.wd0821@gmail.com>
2024-08-16 16:03:52 +0200

    example nr1 - add diff elements

--- script.js
+++ script.js
@@ -320,10 +320,28 @@
 const addBtn = document.querySelector(".add");
 const removeBtn = document.querySelector(".remove");
 const toggleBtn = document.querySelector(".toggle");
-const text = document.querySelector(".test");
+const text = document.querySelector("p");
+
+
+
 
 const addClass = () => {
 	text.classList.add("test");
 };
-
 addBtn.addEventListener("click", addClass);
+
+
+
+
+const removeClass = () => {
+	text.classList.remove("test");
+};
+removeBtn.addEventListener("click", removeClass);
+
+
+
+
+const toggleClass = () => {
+	text.classList.toggle("test");
+};
+toggleBtn.addEventListener("click", toggleClass);
