598eb109899f067ce0347f467d52e8472207c168
NikoWD <mikolaj.wd0821@gmail.com>
2024-08-17 16:34:07 +0200

    done second example

--- script.js
+++ script.js
@@ -317,31 +317,102 @@
 // __________________
 // dodawanie i usuwanie class
 
-const addBtn = document.querySelector(".add");
-const removeBtn = document.querySelector(".remove");
-const toggleBtn = document.querySelector(".toggle");
-const text = document.querySelector("p");
+// const addBtn = document.querySelector(".add");
+// const removeBtn = document.querySelector(".remove");
+// const toggleBtn = document.querySelector(".toggle");
+// const text = document.querySelector("p");
 
+// const addClass = () => {
+// 	text.classList.add("test");
+// };
+// addBtn.addEventListener("click", addClass);
 
+// const removeClass = () => {
+// 	text.classList.remove("test");
+// };
+// removeBtn.addEventListener("click", removeClass);
 
+// const toggleClass = () => {
+// 	text.classList.toggle("test");
+// };
+// toggleBtn.addEventListener("click", toggleClass);
+// ____________________________
+// zadanie nr1 stwórz listę i modyfikuj ją
 
-const addClass = () => {
-	text.classList.add("test");
-};
-addBtn.addEventListener("click", addClass);
+// const div = document.querySelector("div");
+// const ulList = document.createElement("ul");
+// div.append(ulList);
 
+// for (let i = 1; i < 11; i++) {
+// 	const liItem = document.createElement("li");
+// 	liItem.textContent = i;
+// 	ulList.append(liItem);
+// }
 
+// console.log(ulList);
 
+// const lastLi = ulList.querySelector("li:last-child");
+// console.log(lastLi);
+// lastLi.textContent = "Jestem ostatnim elementem";
 
-const removeClass = () => {
-	text.classList.remove("test");
-};
-removeBtn.addEventListener("click", removeClass);
+// lastLi.style.fontSize = "24px";
+// lastLi.style.backgroundColor = "royalBlue";
+// lastLi.style.padding = "20px 40px";
+// // lastLi.style.color = "white";
 
+// __________________
+// zadanie nr 2
 
+// // 2. Pobierz w JS paragrafy, przyciski oraz DIV-a square.
 
+// const par1 = document.querySelector(".p1");
+// const par2 = document.querySelector(".p2.show");
+// const btn1 = document.querySelector(".btn.btn1");
+// const btn2 = document.querySelector(".btn.btn2");
+// const square = document.querySelector(".square");
 
-const toggleClass = () => {
-	text.classList.toggle("test");
-};
-toggleBtn.addEventListener("click", toggleClass);
+// console.log(square);
+
+// // 3. Stwórz funkcje:
+
+// // #1: wyloguj w konsoli napisz "cześć",
+
+// const hej = () => {
+// 	console.log("Cześć");
+// };
+
+// // #2: zmień kolor .square na czerwony
+
+// const changeRed = () => {
+// 	square.style.backgroundColor = "red";
+// };
+
+// // #3: zmień kolor square na niebieski ,
+
+// const changeBlue = () => {
+// 	square.style.backgroundColor = "blue";
+// };
+
+// // #4: najpierw dodaj klasę .show do "Paragrafu 1" i usuń klasę .show z "Paragrafu 2", potem zrób na odwrót // (przełącznik do światła).
+
+// // const changeClassPar1 = () => {
+// // 	par1.classList.add("show");
+// // 	par2.classList.remove("show");
+// // };
+// const toggleClassPar2 = () => {
+// 	par1.classList.toggle("show");
+// 	par2.classList.toggle("show");
+// };
+
+// btn1.addEventListener("dblclick", hej);
+// btn2.addEventListener("click", toggleClassPar2);
+// square.addEventListener("mouseover", changeRed);
+// square.addEventListener("mouseout", changeBlue);
+
+// // changeClassPar1();
+
+// // 4. Funkcje mają być odpalane za pomocą:
+// // #1 podwójnego kliknięcia przycisku z burgerem,
+// // #2 wjechania myszą na DIV.square,
+// // #3 zjechania myszą z DIV.square,
+// // #4 kliknięcia w przycisk z pizzą.
